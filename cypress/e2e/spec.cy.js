c9be80727a4624229e986ad10bb4782c50c72cf8
sabine <khun7706@gmail.com>
2022-09-14 17:08:57 +0800

    Modify cypress config

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,4 +2,13 @@ describe("My First Test", () => {
     it("visit localhost", () => {
         cy.visit("http://localhost:3000/");
     });
+    it("click url", () => {
+        cy.contains("Learn React").click();
+    });
+    // it("get app text", () => {
+    //     cy.get(".App").then(($App) => {
+    //         const appText = $App.text(); // 可以在 then 裡面 const/let
+    //         expect(appText).is.eql("Employee List");
+    //     });
+    // });
 });
